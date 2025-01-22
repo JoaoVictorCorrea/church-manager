c92d852fbe9d058d4c27fcc7a1e635ed6ca24a04
João Victor Correa <correa.joaov25@gmail.com>
2025-01-22 16:16:54 -0300

    First Commit Backend

---
+++ church-manager-backend/src/main/java/com/church/manager/ChurchManagerApplication.java
@@ -0,0 +1,13 @@
+package com.church.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurchManagerApplication.class, args);
+	}
+
+}
